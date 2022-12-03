f86c07ce4fc2736970831035d0f10ca8bbada89d
MingiKang1999 <harryk718@gmail.com>
2022-12-03 15:37:53 -0500

    Final

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const authJwt = require('./helpers/jwt');
+//const authJwt = require('./helpers/jwt');
 const errorHandler = require('./helpers/error-handler');
 
 require("dotenv/config");
